974c044c05cdc7fc383649bbca85c21c18aab0b3
nickollasc <36551569+nickollasc@users.noreply.github.com>
2020-09-03 22:25:10 +0100

    Add files via upload

--- raspberry_connect_based_on_mac.sh
+++ raspberry_connect_based_on_mac.sh
@@ -3,10 +3,9 @@
 # Usage: raspi_connect.sh [X] 
 # Depends: x2x package installed only in raspberry
 
-raspi_mac='E:C7:12:C4:8E:FF'
-raspi_passwd='putRaspberryPasswordHere'
+declare -A info=( ['mac']='EE:C7:12:C4:8E:FF' ['user']='pi' ['passwd']='123456' )	# raspberry info
 
-test $( whoami ) != "root" && echo erro: only root can execute this script && exit
+test $( whoami ) != "root" && echo -e "[erro]: only root can execute this script\ntry: sudo $0" && exit
 
 if ! type nmap &> /dev/null || ! type sshpass &> /dev/null; then # install packages if it not installed
   test -e /etc/debian_version && sudo apt install -y nmap sshpass || sudo yum install -y nmap sshpass
@@ -19,41 +18,31 @@ function get_raspi_ip {
   egrep -o '[[:digit:].]{3,}$| [[:xdigit:]:]{6,}'						| # filter only IP and MAC address
   awk 'BEGIN{ m=0 } { if ( $1 ~ /^[[:digit:].]{3,}/ ) m++; print m, $0 }'			| # create a relationship between IP and MAC 
   awk '{ a[$1] = a[$1] FS substr( $0, index( $0,$2 ) ) } END{ for( i in a ) print i a[i] }'	| # print IP and MAC on same line
-  awk '$3 == "'$raspi_mac'" { print $2 }'							  # get raspberry IP based on MAC
-}
-
-function set_ssh_to_use_an_already_established_connection {
-  if [ ! -e ~/.ssh/config ];then
-	cat > ~/.ssh/config <<-EOF
-	ControlMaster auto
-	ControlPath ~/.ssh/control:%h:%p:%r
-	EOF
-  fi
+  awk '$3 == "'$1'" { print $2 }'								  # get raspberry IP based on MAC
 }
 
 function change_keyboard_layout_on_remote_X11 {
   sleep 2 
-  set_ssh_to_use_an_already_established_connection
-  ssh -o StrictHostKeyChecking=no -X pi@$raspi_ip DISPLAY=:0 'setxkbmap pt'
+  sshpass -p "${info['passwd']}" ssh -o StrictHostKeyChecking=no -X ${info['user']}@$ip DISPLAY=:0 'setxkbmap pt' &
   reset
   echo 'X11 raspberry connection ready, move mouse pointer to other Monitor/TV!'
 }
 
-raspi_ip=$( get_raspi_ip ) 
+ip=$( get_raspi_ip ${info['mac']} )	# get raspberry IP passing mac as parameter
 
-test -z $raspi_ip && echo erro: IP not found for this Mac: $raspi_mac && exit
+test -z $ip && echo erro: IP not found for this Mac: ${info['mac']} && exit
 
 if [ "$1" == "X" ] && [ -e ~/.ssh/pi_rsa ]; then # connect to X11 on raspberry using ssh key
-  ssh -i ~/.ssh/pi_rsa -o StrictHostKeyChecking=no -X pi@$raspi_ip x2x -north -north -to :0 &
+  ssh -i ~/.ssh/pi_rsa -o StrictHostKeyChecking=no -X ${info['user']}@$ip x2x -north -north -to :0 &
   change_keyboard_layout_on_remote_X11
 
 elif [ "$1" == "X" ] && [ ! -e ~/.ssh/pi_rsa ]; then # connect to X11 on raspberry using sshpass
-  sshpass -p "$raspi_passwd" ssh -o StrictHostKeyChecking=no -X pi@$raspi_ip x2x -north -north -to :0 &
+  sshpass -p "${info['passwd']}" ssh -o StrictHostKeyChecking=no -X ${info['user']}@$ip x2x -north -north -to :0 &
   change_keyboard_layout_on_remote_X11
 
 elif [ -e ~/.ssh/pi_rsa ];then # connect to raspberry and use terminal using ssh key
-  ssh -i ~/.ssh/pi_rsa -o StrictHostKeyChecking=no pi@$raspi_ip
+  ssh -i ~/.ssh/pi_rsa -o StrictHostKeyChecking=no ${info['user']}@$ip
 
 else # connect to raspberry and use terminal using sshpass
-  sshpass -p "$raspi_passwd" ssh -o StrictHostKeyChecking=no pi@$raspi_ip 
+  sshpass -p "${info['passwd']}" ssh -o StrictHostKeyChecking=no ${info['user']}@$ip 
 fi
